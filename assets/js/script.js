3c918172e9b3640bae3cf8e0bea3888a65adcc82
Afkeru <451345424@qq.com>
2023-08-24 12:27:27 +0800

    修复按键不能设置成空格的bug,将协议改成https，更新service-worker

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,7 +16,7 @@ import {
 } from './settingsManager.js';
 
 const status = new Info();
-const serverIp = "http://git.afkeru.top";
+const serverIp = "https://git.afkeru.top";
 let currentServer = null;
 let defaultServers = [{
         name: '本地1',
@@ -732,14 +732,23 @@ saveButton.addEventListener('click', () => {
 
 window.addEventListener('keydown', (event) => {
     if (activeButton) {
-        activeButton.textContent = event.key.toUpperCase();
+        event.preventDefault(); // 阻止默认行为
+
+        let keyName = event.key;
+        if (keyName === ' ') { // 如果是空格键
+            keyName = 'SPACE'; // 修改为 SPACE
+        }
+
+        activeButton.textContent = keyName.toUpperCase();
         activeButton.classList.remove('glow');
 
         let keybindId = activeButton.id;
         if (keybindId.startsWith('keybind-')) {
             keybindId = keybindId.substring('keybind-'.length);
         }
-        keyManager.setKeybind(keybindId, event.key);
+
+        keyManager.setKeybind(keybindId, keyName); 
+
         console.log(keyManager.getAllKeybinds());
         activeButton = null;
     }
